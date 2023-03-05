b250b5013e8b4602bf7f69c5c9249c8b46df61a5
ptar124 <party48235@ymail.com>
2023-03-05 23:08:57 +0000

    filter logic (formula by chatgpt will check later)

--- src/osc.cpp
+++ src/osc.cpp
@@ -2,30 +2,52 @@
 #include <iostream>
 #include <math.h>
 
-float pi = 22/7;
+double pi = 22/7;
 
 class Waveform {
     public:
-        std::vector<float> wave;
-        float frequency = 440;
-        float samples = 22000/frequency;
+        std::vector<double> wave;
+        double frequency = 440;
+        double samples = 22000/frequency;
 
-    void generateSine(){
-        wave.clear();
-        for (int i = 0; i < samples; i++){
-            wave[i] = sin((i*2*pi)/samples);
-        }   
-    }
+        void generateSine(){
+            wave.clear();
+            for (int i = 0; i < samples; i++){
+                wave[i] = sin((i*2*pi)/samples);
+            }   
+        }
 };
 
 class Oscillator {
     public:
         Waveform oscWaveform;
-        float envAtt = 0;
-        float envDel = 0;
-        float envSus = 0;
-        float envRel = 0;
+        double envAtt = 0;
+        double envDel = 0;
+        double envSus = 0;
+        double envRel = 0;
         Waveform LFOWaveform;
+};
+
+class Filter {
+    public:
+        double prevInput = 0;
+        double thisInput = 0;
+        double prevOutput = 0;
+        double thisOutput = 0;
+        double R = 1000;
+        double C = 0.001;
+        double tau = R*C;
+        double dt = 1/22000;
+
+        double lowPass(double input){
+            thisOutput = (tau*prevOutput + dt*thisInput) / (tau + dt);
+            return thisOutput;
+        }
+
+        double highPass(double input){
+            thisOutput = (1 - (tau/(2*dt) - 1)/(tau/(2*dt) + 1)*(prevOutput + input - prevInput));
+            return thisOutput;
+        }
 };
 
 int main()

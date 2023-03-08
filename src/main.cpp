da2857464cb3b3f812495c4754b7ce8ba30b1d5d
mc1017 <marco.chan616@gmail.com>
2023-03-08 12:21:14 +0000

    Music playing

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,198 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 
-//Constants
-  const uint32_t interval = 100; //Display update interval
+// Constants
+const uint32_t interval = 100; // Display update interval
 
-//Pin definitions
-  //Row select and enable
-  const int RA0_PIN = D3;
-  const int RA1_PIN = D6;
-  const int RA2_PIN = D12;
-  const int REN_PIN = A5;
+// Pin definitions
+// Row select and enable
+const int RA0_PIN = D3;
+const int RA1_PIN = D6;
+const int RA2_PIN = D12;
+const int REN_PIN = A5;
 
-  //Matrix input and output
-  const int C0_PIN = A2;
-  const int C1_PIN = D9;
-  const int C2_PIN = A6;
-  const int C3_PIN = D1;
-  const int OUT_PIN = D11;
+// Matrix input and output
+const int C0_PIN = A2;
+const int C1_PIN = D9;
+const int C2_PIN = A6;
+const int C3_PIN = D1;
+const int OUT_PIN = D11;
 
-  //Audio analogue out
-  const int OUTL_PIN = A4;
-  const int OUTR_PIN = A3;
+// Audio analogue out
+const int OUTL_PIN = A4;
+const int OUTR_PIN = A3;
 
-  //Joystick analogue in
-  const int JOYY_PIN = A0;
-  const int JOYX_PIN = A1;
+// Joystick analogue in
+const int JOYY_PIN = A0;
+const int JOYX_PIN = A1;
 
-  //Output multiplexer bits
-  const int DEN_BIT = 3;
-  const int DRST_BIT = 4;
-  const int HKOW_BIT = 5;
-  const int HKOE_BIT = 6;
+// Output multiplexer bits
+const int DEN_BIT = 3;
+const int DRST_BIT = 4;
+const int HKOW_BIT = 5;
+const int HKOE_BIT = 6;
 
-//Display driver object
+// Look up current step size
+volatile uint32_t currentStepSize;
+
+// Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
-//Function to set outputs using key matrix
-void setOutMuxBit(const uint8_t bitIdx, const bool value) {
-      digitalWrite(REN_PIN,LOW);
-      digitalWrite(RA0_PIN, bitIdx & 0x01);
-      digitalWrite(RA1_PIN, bitIdx & 0x02);
-      digitalWrite(RA2_PIN, bitIdx & 0x04);
-      digitalWrite(OUT_PIN,value);
-      digitalWrite(REN_PIN,HIGH);
-      delayMicroseconds(2);
-      digitalWrite(REN_PIN,LOW);
+void sampleISR()
+{
+    static uint32_t phaseAcc = 0;
+    phaseAcc += currentStepSize;
+    int32_t Vout = (phaseAcc >> 24) - 128;
+    analogWrite(OUTR_PIN, Vout + 128);
 }
 
-void setup() {
-  // put your setup code here, to run once:
+void setRow(uint8_t rowIdx)
+{
+    // Set row select enable pin (REN) high
+    digitalWrite(REN_PIN, LOW);
+    digitalWrite(RA0_PIN, LOW);
+    digitalWrite(RA1_PIN, LOW);
+    digitalWrite(RA2_PIN, LOW);
+    // Read inputs from column 0
+    uint8_t bit_value0 = ((rowIdx >> 0) & 1);
+    uint8_t bit_value1 = ((rowIdx >> 1) & 1);
+    uint8_t bit_value2 = ((rowIdx >> 2) & 1);
+    if (bit_value0)
+    {
+        digitalWrite(RA0_PIN, HIGH);
+    }
+    if (bit_value1)
+    {
+        digitalWrite(RA1_PIN, HIGH);
+    }
+    if (bit_value2)
+    {
+        digitalWrite(RA2_PIN, HIGH);
+    }
+    // Set row select enable pin (REN) high
+    digitalWrite(REN_PIN, HIGH);
+}
+uint8_t readCols()
+{
 
-  //Set pin directions
-  pinMode(RA0_PIN, OUTPUT);
-  pinMode(RA1_PIN, OUTPUT);
-  pinMode(RA2_PIN, OUTPUT);
-  pinMode(REN_PIN, OUTPUT);
-  pinMode(OUT_PIN, OUTPUT);
-  pinMode(OUTL_PIN, OUTPUT);
-  pinMode(OUTR_PIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+    uint8_t C0 = digitalRead(C0_PIN);
+    uint8_t C1 = digitalRead(C1_PIN);
+    uint8_t C2 = digitalRead(C2_PIN);
+    uint8_t C3 = digitalRead(C3_PIN);
 
-  pinMode(C0_PIN, INPUT);
-  pinMode(C1_PIN, INPUT);
-  pinMode(C2_PIN, INPUT);
-  pinMode(C3_PIN, INPUT);
-  pinMode(JOYX_PIN, INPUT);
-  pinMode(JOYY_PIN, INPUT);
+    digitalWrite(REN_PIN, LOW);
 
-  //Initialise display
-  setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
-  delayMicroseconds(2);
-  setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
-  u8g2.begin();
-  setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
-
-  //Initialise UART
-  Serial.begin(9600);
-  Serial.println("Hello World");
+    // Concatenate the four bits into a single byte and return
+    return (C3 << 3) | (C2 << 2) | (C1 << 1) | C0;
+}
+// Function to set outputs using key matrix
+void setOutMuxBit(const uint8_t bitIdx, const bool value)
+{
+    digitalWrite(REN_PIN, LOW);
+    digitalWrite(RA0_PIN, bitIdx & 0x01);
+    digitalWrite(RA1_PIN, bitIdx & 0x02);
+    digitalWrite(RA2_PIN, bitIdx & 0x04);
+    digitalWrite(OUT_PIN, value);
+    digitalWrite(REN_PIN, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(REN_PIN, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  static uint32_t next = millis();
-  static uint32_t count = 0;
+int highest_unset_bit(uint8_t array[])
+{
+    uint16_t merged = ((uint16_t)array[2] & 0x0F) << 8 |
+                      ((uint16_t)array[1] & 0x0F) << 4 |
+                      ((uint16_t)array[0] & 0x0F);
+    int index = 0;
+    uint16_t mask = 0x800;
+    int i;
+    for (i = 11; i >= 0; i--)
+    {
+        if ((merged & mask) == 0)
+        {
+            break;
+        }
+        mask >>= 1;
+    }
+    return i;
+}
 
-  if (millis() > next) {
+void setup()
+{
+    // put your setup code here, to run once:
+
+    // Set pin directions
+    pinMode(RA0_PIN, OUTPUT);
+    pinMode(RA1_PIN, OUTPUT);
+    pinMode(RA2_PIN, OUTPUT);
+    pinMode(REN_PIN, OUTPUT);
+    pinMode(OUT_PIN, OUTPUT);
+    pinMode(OUTL_PIN, OUTPUT);
+    pinMode(OUTR_PIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    pinMode(C0_PIN, INPUT);
+    pinMode(C1_PIN, INPUT);
+    pinMode(C2_PIN, INPUT);
+    pinMode(C3_PIN, INPUT);
+    pinMode(JOYX_PIN, INPUT);
+    pinMode(JOYY_PIN, INPUT);
+
+    // Initialise display
+    setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
+    delayMicroseconds(2);
+    setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
+    u8g2.begin();
+    setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
+    TIM_TypeDef *Instance = TIM1;
+    HardwareTimer *sampleTimer = new HardwareTimer(Instance);
+    sampleTimer->setOverflow(22000, HERTZ_FORMAT);
+    sampleTimer->attachInterrupt(sampleISR);
+    sampleTimer->resume();
+    // Initialise UART
+    Serial.begin(9600);
+    Serial.println("Hello World");
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    static uint32_t next = millis();
+    static uint32_t count = 0;
+    uint8_t keyArray[7];
+    int highestBit = -1;
+    const uint32_t stepSizes[] = {
+        51076057, 54113197, 57330935, 60740010, 64351799, 68178356, 72232452, 76527617, 81078186, 85899346, 91007187, 96418756 // G# / Ab (830.61 Hz)
+    };
+
+    while (millis() < next)
+        ; // Wait for next interval
     next += interval;
 
-    //Update display
-    u8g2.clearBuffer();         // clear the internal memory
+    // Update display
+    u8g2.clearBuffer();                 // clear the internal memory
     u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-    u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
-    u8g2.setCursor(2,20);
-    u8g2.print(count++);
-    u8g2.sendBuffer();          // transfer internal memory to the display
-
-    //Toggle LED
+    for (uint8_t i = 0; i < 3; i++)
+    {
+        setRow(i);
+        delayMicroseconds(3);
+        keyArray[i] = readCols();
+        u8g2.setCursor(2 + 10 * i, 20);
+        u8g2.print(keyArray[i], HEX);
+        highestBit = highest_unset_bit(keyArray);
+        if (highestBit < 0)
+        {
+            currentStepSize = 0;
+        }
+        else
+        {
+            currentStepSize = stepSizes[highestBit];
+        }
+        u8g2.setCursor(2, 10);
+        u8g2.print(currentStepSize, HEX);
+    }
+    // u8g2.print(currentStepSize, HEX);
+    u8g2.sendBuffer(); // transfer internal memory to the display
+    // Toggle LED
     digitalToggle(LED_BUILTIN);
-  }
 }

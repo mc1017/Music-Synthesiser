935a184f26548542801a678fcac2301b5f2e69d3
mc1017 <marco.chan616@gmail.com>
2023-03-23 21:07:46 +0000

    measured timings

--- src/main.cpp
+++ src/main.cpp
@@ -18,25 +18,12 @@ SemaphoreHandle_t CAN_TXSemaphore;
 QueueHandle_t msgInQ;
 QueueHandle_t msgOutQ;
 
-// ALTERNATE WAVEFORM(Triangle)
-/*
-void sampleISR(){
-  static uint32_t phaseAcc = 0;
-  phaseAcc += currentStepSize;
-  int32_t Vout = 0;
-  if ((phaseAcc >> 24) < 128){
-    Vout = (phaseAcc >> 24) - 128;
-  }
-  else{
-    Vout = 255 - (phaseAcc >> 24);
-  }
-  analogWrite(OUTR_PIN, Vout + 128);
-}
-*/
+#define DISABLE_THREADS
+#define TEST_DISPLAY
 
 void sampleISR()
 {
-
+    uint32_t startTime = micros();
     // Calculate the output for each active note
     uint8_t activeNotes = 0;
     int32_t Vout = 0;
@@ -60,51 +47,21 @@ void sampleISR()
         Vout = Vout / activeNotes;
     }
     analogWrite(OUTR_PIN, Vout + 128);
+    Serial.println(micros() - startTime);
 }
-
-// ALTERNATE WAVEFORM(Square)
-// void sampleISR()
-// {
-//     static uint32_t phaseAcc = 0;
-//     phaseAcc += currentStepSize;
-//     int32_t Vout = 0;
-//     if ((phaseAcc >> 24) < 128)
-//     {
-//         Vout = 0 - 128;
-//     }
-//     else
-//     {
-//         Vout = 255 - 128;
-//     }
-//     Vout = Vout >> (10 - knob[3].getRotation());
-//     analogWrite(OUTR_PIN, Vout + 128);
-// }
-
-// ALTERNATE WAVEFORM(Sine, to be optimised)
-/*
-void sampleISR(){
-  static uint32_t phaseAcc = 0;
-  phaseAcc += currentStepSize;
-  double radians = phaseAcc/MAX_UINT32;
-  double sineScaled = std::sin(radians)*2147483646 + 0.5;
-  int32_t sineScaledFixed = static_cast<int32_t>(sineScaled);
-
-  int32_t Vout = (sineScaledFixed >> 24) - 128;
-  analogWrite(OUTR_PIN, Vout + 128);
-}
-*/
 
 void CAN_RX_ISR(void)
 {
     uint8_t RX_Message_ISR[8];
     uint32_t ID;
-    CAN_RX(ID, RX_Message_ISR);
-    xQueueSendFromISR(msgInQ, RX_Message_ISR, NULL);
+    // CAN_RX(ID, RX_Message_ISR);
+
+    // xQueueSendFromISR(msgInQ, RX_Message_ISR, NULL);
 }
 
 void CAN_TX_ISR(void)
 {
-    xSemaphoreGiveFromISR(CAN_TXSemaphore, NULL);
+    // xSemaphoreGiveFromISR(CAN_TXSemaphore, NULL);
 }
 
 // CAN Recieve Task
@@ -112,15 +69,13 @@ void Can_RX_Task(void *pvParameters)
 {
     uint8_t tmp_RX_Message[8] = {0};
 
-    while (1)
+    for (int i = 0; i < 1; i++)
     {
-        xQueueReceive(msgInQ, tmp_RX_Message, portMAX_DELAY);
-        xSemaphoreTake(RX_MessageMutex, portMAX_DELAY);
+
         for (int i = 0; i < 8; i++)
         {
             RX_Message[i] = tmp_RX_Message[i];
         }
-        xSemaphoreGive(RX_MessageMutex);
     }
 }
 
@@ -128,10 +83,10 @@ void Can_RX_Task(void *pvParameters)
 void CAN_TX_Task(void *pvParameters)
 {
     uint8_t msgOut[8];
-    while (1)
+    for (int i = 0; i < 1; i++)
     {
-        xQueueReceive(msgOutQ, msgOut, portMAX_DELAY);
-        xSemaphoreTake(CAN_TXSemaphore, portMAX_DELAY);
+        // xQueueReceive(msgOutQ, msgOut, portMAX_DELAY);
+        // xSemaphoreTake(CAN_TXSemaphore, portMAX_DELAY);
         CAN_TX(0x111, msgOut);
     }
 }
@@ -152,8 +107,8 @@ void canBusInitRoutine()
 
     status = CAN_Start();
 
-    msgInQ = xQueueCreate(36, 8);
-    msgOutQ = xQueueCreate(36, 8);
+    msgInQ = xQueueCreate(384, 8);
+    msgOutQ = xQueueCreate(384, 8);
 }
 
 void scanKeysTask(void *pvParameters)
@@ -165,13 +120,12 @@ void scanKeysTask(void *pvParameters)
     uint8_t test_array[5] = {0};
     uint8_t TX_Message[8] = {0};
 
-    while (1)
+    for (int count = 0; count < 1; count++)
     {
-        vTaskDelayUntil(&xLastWakeTime, xFrequency);
 
         if (!transmitter && multipleModule)
         {
-            xSemaphoreTake(RX_MessageMutex, portMAX_DELAY);
+
             for (int i = 0; i < 8; i++)
             {
                 tmp_RX[i] = RX_Message[i];
@@ -180,7 +134,6 @@ void scanKeysTask(void *pvParameters)
                 octave = 0;
             else
                 octave = RX_Message[5];
-            xSemaphoreGive(RX_MessageMutex);
         }
         else if (transmitter && multipleModule)
         {
@@ -191,7 +144,6 @@ void scanKeysTask(void *pvParameters)
         {
             setRow(i);
             delayMicroseconds(3);
-            xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
             keyArray[i] = readCols();
             TX_Message[i] = keyArray[i];
             if (i < 3)
@@ -213,38 +165,24 @@ void scanKeysTask(void *pvParameters)
             uint8_t currentB2 = (keyArray[i] & 0b00001000) >> 3;
             uint8_t currentA3 = keyArray[i] & 0b00000001;
             uint8_t currentB3 = (keyArray[i] & 0b00000010) >> 1;
-            xSemaphoreTake(rotationMutex, portMAX_DELAY);
             switch (i)
             {
             case 3:
                 knob[3].updateRotation(currentA3, currentB3);
-                // u8g2.setCursor(80, 20);
-                // u8g2.print(knob[3].getRotation());
 
                 knob[2].updateRotation(currentA2, currentB2);
-                // u8g2.setCursor(65, 20);
-                // u8g2.print(knob[2].getRotation());
 
                 break;
             case 4:
                 knob[1].updateRotation(currentA1, currentB1);
-                // u8g2.setCursor(50, 20);
-                // u8g2.print(knob[1].getRotation());
 
                 knob[0].updateRotation(currentA0, currentB0);
-                // u8g2.setCursor(35, 20);
-                // u8g2.print(knob[0].getRotation());
-
                 break;
             case 5:
 
             default:
-                // u8g2.setCursor(2 + 10 * i, 20);
-                // u8g2.print(keyArray[i], HEX);
                 break;
             }
-            xSemaphoreGive(rotationMutex);
-            xSemaphoreGive(keyArrayMutex);
         }
         TX_Message[5] = position;
 
@@ -254,17 +192,6 @@ void scanKeysTask(void *pvParameters)
         {
             xQueueSend(msgOutQ, TX_Message, portMAX_DELAY);
         }
-
-        // if (!transmitter && multipleModule)
-        // {
-        //     u8g2.setCursor(60, 10);
-        //     u8g2.print(RX_Message[5]);
-        //     u8g2.print(RX_Message[4]);
-        //     u8g2.print(RX_Message[3]);
-        //     u8g2.print(RX_Message[2]);
-        //     u8g2.print(RX_Message[1]);
-        //     u8g2.print(RX_Message[0]);
-        // }
     }
 }
 
@@ -293,27 +220,14 @@ void updateDisplayTask(void *pvParameters)
     const TickType_t xFrequency = 100 / portTICK_PERIOD_MS;
     TickType_t xLastWakeTime = xTaskGetTickCount();
 
-    // if (transmitter && multipleModule)
-    //     uint8_t TX_Message[8] = {0};
-    // TX_Message[0] = 7;
-    // TX_Message[1] = 6;
-    // TX_Message[2] = 4;
-    // TX_Message[3] = 8;
-    // TX_Message[4] = 9;
-    // TX_Message[5] = 9;
-    // TX_Message[6] = 9;
-    // TX_Message[7] = 9;
-
-    while (1)
+    for (int i = 0; i < 1; i++)
     {
         u8g2.clearBuffer();
         u8g2.setFont(u8g2_font_t0_11_tf); // choose a suitable font
         const uint32_t *stepSizes = stepSizeList[octave];
-        vTaskDelayUntil(&xLastWakeTime, xFrequency);
         // Update display
-        xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+
         highestBit = highest_unset_bit(keyArray);
-        xSemaphoreGive(keyArrayMutex);
         __atomic_store_n(&currentStepSize, (highestBit < 0) ? 0 : stepSizes[highestBit], __ATOMIC_SEQ_CST);
         displayTXRX(transmitter, multipleModule, position);
         displayVolume(knob[3].getRotation());
@@ -336,7 +250,9 @@ void updateDisplayTask(void *pvParameters)
 
 void setup()
 {
-
+    Serial.begin(9600);
+    delay(1000);
+    Serial.println("Reached Here 2");
     // Set pin directions
     pinMode(RA0_PIN, OUTPUT);
     pinMode(RA1_PIN, OUTPUT);
@@ -370,7 +286,7 @@ void setup()
 
     canBusInitRoutine();
 
-    multipleModule = handshakeRoutine(position);
+    // multipleModule = handshakeRoutine(position);
 
     // Resend Handshake End signal to ensure all modules exit the loop
     delay(100);
@@ -388,15 +304,7 @@ void setup()
         transmitter = false;
     else
         transmitter = true;
-    TaskHandle_t scanKeysHandle = NULL;
-    xTaskCreate(
-        scanKeysTask, /* Function that implements the task */
-        "scanKeys",   /* Text name for the task */
-        500,          /* Stack size in words, not bytes */
-        NULL,         /* Parameter passed into the task */
-        2,            /* Task priority */
-        &scanKeysHandle);
-
+    Serial.println("Reached Here 1");
     TaskHandle_t updateDisplayHandle = NULL;
     xTaskCreate(
         updateDisplayTask, /* Function that implements the task */
@@ -405,28 +313,91 @@ void setup()
         NULL,              /* Parameter passed into the task */
         1,                 /* Task priority */
         &updateDisplayHandle);
+
+#ifndef DISABLE_THREADS
+    TaskHandle_t CanSendHandle = NULL;
+    xTaskCreate(
+        CAN_TX_Task, /* Function that implements the task */
+        "CAN_TX",    /* Text name for the task */
+        200,         /* Stack size in words, not bytes */
+        NULL,        /* Parameter passed into the task */
+        3,           /* Task priority */
+        &CanSendHandle);
+    TaskHandle_t CanReceiveHandle = NULL;
+    xTaskCreate(
+        Can_RX_Task, /* Function that implements the task */
+        "Can_RX",    /* Text name for the task */
+        200,         /* Stack size in words, not bytes */
+        NULL,        /* Parameter passed into the task */
+        3,           /* Task priority */
+        &CanReceiveHandle);
+
+    TaskHandle_t scanKeysHandle = NULL;
+    xTaskCreate(
+        scanKeysTask, /* Function that implements the task */
+        "scanKeys",   /* Text name for the task */
+        500,          /* Stack size in words, not bytes */
+        NULL,         /* Parameter passed into the task */
+        2,            /* Task priority */
+        &scanKeysHandle);
     if (multipleModule && transmitter)
     {
-        TaskHandle_t CanSendHandle = NULL;
-        xTaskCreate(
-            CAN_TX_Task, /* Function that implements the task */
-            "CAN_TX",    /* Text name for the task */
-            200,         /* Stack size in words, not bytes */
-            NULL,        /* Parameter passed into the task */
-            3,           /* Task priority */
-            &CanSendHandle);
     }
     else if (multipleModule && !transmitter)
     {
-        TaskHandle_t CanReceiveHandle = NULL;
-        xTaskCreate(
-            Can_RX_Task, /* Function that implements the task */
-            "Can_RX",    /* Text name for the task */
-            200,         /* Stack size in words, not bytes */
-            NULL,        /* Parameter passed into the task */
-            3,           /* Task priority */
-            &CanReceiveHandle);
     }
+#endif
+#ifdef TEST_SCANKEYS
+    uint32_t startTime = micros();
+    for (int iter = 0; iter < 32; iter++)
+    {
+        scanKeysTask(&scanKeysHandle);
+    }
+    Serial.println(micros() - startTime);
+    while (1)
+        ;
+#endif
+#ifdef TEST_DISPLAY
+    uint32_t startTime = micros();
+    for (int iter = 0; iter < 32; iter++)
+    {
+        updateDisplayTask(&updateDisplayHandle);
+    }
+    Serial.println(micros() - startTime);
+    while (1)
+        ;
+#endif
+#ifdef TEST_CAN_RX
+    uint32_t startTime = micros();
+    for (int iter = 0; iter < 36; iter++)
+    {
+        Can_RX_Task(&CanReceiveHandle);
+    }
+    Serial.println(micros() - startTime);
+    while (1)
+        ;
+#endif
+#ifdef TEST_CAN_TX
+    uint32_t startTime = micros();
+    for (int iter = 0; iter < 36; iter++)
+    {
+        CAN_TX_Task(&CanSendHandle);
+    }
+    Serial.println(micros() - startTime);
+    while (1)
+        ;
+#endif
+
+#ifdef TEST_IR
+
+    for (int iter = 0; iter < 36; iter++)
+    {
+        CAN_RX_ISR();
+    }
+
+    while (1)
+        ;
+#endif
     vTaskStartScheduler();
 }
 
